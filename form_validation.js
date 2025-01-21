a99bb690be155163334524d6b8eaa5d813af1833
Sk7867 <siddhesh.kamble7867@gmail.com>
2025-01-21 20:31:29 +0530

    Form validation and error display done

--- form_validation.js
+++ form_validation.js
@@ -1,10 +1,39 @@
 const form = document.getElementById('form');
 const formInput = document.getElementById('form_input');
 const formSubmitBtn = document.getElementById('form_submit');
-const formErrorElement = document.getElementById('form_error')
+const formErrorElements = document.getElementsByClassName('form_error')
 
-formSubmitBtn.addEventListener('click', validateForm)
+formSubmitBtn.addEventListener('click', (e) => {validateForm(e)})
 
-function validateForm(){
-  console.log('Hello There')
+function validateForm(e){
+  e.preventDefault()
+  let emailValue = formInput.value
+  if(emailValue){
+    let formCheck = checkandValidateEmailPattern(emailValue)
+    if(formCheck){
+      alert('Thank You for Subscribing!')
+    }
+  }else{
+    displayFormError() 
+  }
+}
+
+// Function to validate Email Id
+function checkandValidateEmailPattern(email){
+  let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+  if(!emailPattern.test(email)){
+    displayFormError()
+    formInput.value = '';
+    emailValue = ''
+    return false
+  }else{
+    return true
+  }
+}
+
+//Function to display form errors
+function displayFormError(){
+  for (let index = 0; index < formErrorElements.length; index++) {
+    formErrorElements[index].style.display = 'block'
+  }
 }
